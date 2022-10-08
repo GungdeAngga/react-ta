25fc48c871822434df8404fb317153e9efe54acc
GungdeAngga <agungangga2001@gmail.com>
2022-10-08 16:34:37 +0800

    Footer component done

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import FooterWeb from '../components/FooterWeb'
 import HeaderWeb from '../components/HeaderWeb'
 
 export default function HomePage() {
   return (
+    <div>
     <HeaderWeb/>
+    <FooterWeb/>
+    </div>
   )
 }
