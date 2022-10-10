1849ef0e3eafb19036bd51dbdab58a8275744384
Angga Aditya <agungangga2001@gmail.com>
2022-10-10 18:28:46 +0800

    change witdh button

--- src/components/Footer/FooterWeb.js
+++ src/components/Footer/FooterWeb.js
@@ -4,47 +4,47 @@ import "./FooterWeb.css"
 
 const style = {
     footer: {
-        position: "absolute", 
-        bottom: "0", 
+        position: "absolute",
+        bottom: "0",
         left: "0",
-        width:"100%",
+        width: "100%",
         marginTop: "auto",
         backgroundColor: "#484848",
     },
-    footerContent:{
+    footerContent: {
         margin: "auto",
         width: "600px",
     },
-    Button:{
-        width: "21.5%", 
-        marginTop: "20px", 
+    Button: {
+        width: "18%",
+        marginTop: "20px",
         paddingLeft: "80px",
     }
 }
 
 export default function FooterWeb() {
-  return (
-    <div style={style.footer}>
-        <div style={style.footerContent}>
-            <div className='btn-group-footer' style={{width: "100%",}}>
+    return (
+        <div style={style.footer}>
+            <div style={style.footerContent}>
+                <div className='btn-group-footer' style={{ width: "100%", }}>
 
-                <Link to="/" className='btn' >
-                    <img src='./assets/home-free-icon-font.png' alt="home" style={style.Button}/>
-                    <span className='caption' style={{paddingLeft: "80px",}}>Home</span>
-                </Link>
+                    <Link to="/" className='btn' >
+                        <img src='./assets/home-free-icon-font.png' alt="home" style={style.Button} />
+                        <span className='caption' style={{ paddingLeft: "80px", }}>Home</span>
+                    </Link>
 
-                <Link to="/" className='btn' >
-                    <img src='./assets/time-past-free-icon-font.png' alt="History" style={style.Button}/>
-                    <span className='caption' style={{paddingLeft: "75px",}}>History</span>
-                </Link>
+                    <Link to="/" className='btn' >
+                        <img src='./assets/time-past-free-icon-font.png' alt="History" style={style.Button} />
+                        <span className='caption' style={{ paddingLeft: "75px", }}>History</span>
+                    </Link>
 
-                <Link to="/" className='btn' >
-                    <img src='./assets/portrait-free-icon-font.png' alt="Account" style={style.Button}/>
-                    <span className='caption' style={{paddingLeft: "72px",}}>Account</span>
-                </Link>
+                    <Link to="/" className='btn' >
+                        <img src='./assets/portrait-free-icon-font.png' alt="Account" style={style.Button} />
+                        <span className='caption' style={{ paddingLeft: "72px", }}>Account</span>
+                    </Link>
 
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
