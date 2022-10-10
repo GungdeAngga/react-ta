a16d71fdf47b782bb6550bc922a5cecac499488a
Angga Aditya <agungangga2001@gmail.com>
2022-10-10 14:10:36 +0800

    transfer content done

---
+++ src/views/TransferPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import TransferWeb from '../components/contents/TransferContent'
+import FooterWeb from '../components/Footer/FooterWeb'
+import HeaderWeb from '../components/HeaderWeb'
+
+export default function TransferPage() {
+  return (
+    <div>
+        <HeaderWeb/>
+        <TransferWeb/>
+        <FooterWeb/>
+    </div>
+  )
+}
